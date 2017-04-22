15a45adfda7620fde65ba7a1848c3e6649cb5bbf
Sander in 't Veld <sanderintveld@hotmail.com>
2017-04-22 20:24:39 +0200

    Added my cube (v11), downloaded to cache.

--- cube.rb
+++ cube.rb
@@ -2,8 +2,39 @@
 
 require "mtg_sdk"
 
-cards = MTG::Set.generate_booster("ogw")
+cardnames = []
 
-cards.each do |card|
-	puts card.name
+File.open("cube.txt").each do |line|
+	line.strip!
+	if line.empty?
+		next
+	end
+	if line.start_with? "//"
+		next
+	end
+	cardnames << line
+end
+
+cardnames.each do |cardname|
+
+	filename = "cache/" + cardname + ".json"
+
+	if File.file? filename
+		next
+	end
+
+	print "Downloading #{cardname}... \t"
+
+	cards = MTG::Card
+			.where(name: cardname)
+			.where(pageSize: 1)
+			.all
+
+	card = cards[0]
+
+	File.open(filename, 'w') do |line|
+		line.puts card.to_json
+	end
+
+	print "OK\n"
 end
